0cc29429f31f04d6ef433049716c2ed6fd6801e3
kwak <kwak@dualsoft.com>
2025-09-09 06:58:53 +0900

    .

--- src/engine/Ev2.Core.FS/TypeConversion,Serialization/DsCopy.Properties.fs
+++ src/engine/Ev2.Core.FS/TypeConversion,Serialization/DsCopy.Properties.fs
@@ -25,6 +25,20 @@ module internal DsCopyModule =
 
 
     /// 추후 참조를 위해, XXX.{Rt,Nj,ORM}Object 에 알맞은 값을 설정한다.
+    let private copyFlowProperties (sbx: obj) (dbx: obj) =
+        let s =
+            match sbx with
+            | :? DsSystemEntityWithFlow  as s -> {| FlowGuid=s.FlowGuid; FlowId=s.FlowId |}
+            | :? NjSystemEntityWithFlow  as s -> {| FlowGuid=s.FlowGuid |> Option.ofObj |-> s2guid; FlowId=None |}
+            | :? ORMSystemEntityWithFlow as s -> {| FlowGuid=None; FlowId=s.FlowId |}
+            | _ -> failwith "ERROR"
+
+        match dbx with
+        | :? DsSystemEntityWithFlow  as d -> d.FlowGuid<-s.FlowGuid; d.FlowId<-s.FlowId
+        | :? NjSystemEntityWithFlow  as d -> d.FlowGuid<-s.FlowGuid |-> guid2str |> Option.toObj
+        | :? ORMSystemEntityWithFlow as d -> d.FlowId<-s.FlowId
+        | _ -> failwith "ERROR"
+
     let private linkUniq (src:#Unique) (dst:#Unique): #Unique=
         match box src with
         | :? IRtUnique  as s -> dst.RtObject  <- Some s
@@ -155,61 +169,11 @@ module internal DsCopyModule =
 
 
 
-        | (:? DsButton) | (:? NjButton) | (:? ORMButton) ->
-            let s =
-                match sbx with
-                | :? DsButton  as s -> {| FlowGuid=s.FlowGuid; FlowId=s.FlowId |}
-                | :? NjButton  as s -> {| FlowGuid=s.FlowGuid |> Option.ofObj |-> s2guid; FlowId=None |}
-                | :? ORMButton as s -> {| FlowGuid=None; FlowId=s.FlowId |}
-                | _ -> failwith "ERROR"
-            
-            match dbx with
-            | :? DsButton  as d -> d.FlowGuid<-s.FlowGuid; d.FlowId<-s.FlowId
-            | :? NjButton  as d -> d.FlowGuid<-s.FlowGuid |-> guid2str |> Option.toObj
-            | :? ORMButton as d -> d.FlowId<-s.FlowId
-            | _ -> failwith "ERROR"
-
-        | (:? Lamp) | (:? NjLamp) | (:? ORMLamp) ->
-            let s =
-                match sbx with
-                | :? Lamp    as s -> {| FlowGuid=s.FlowGuid; FlowId=s.FlowId |}
-                | :? NjLamp  as s -> {| FlowGuid=s.FlowGuid |> Option.ofObj |-> s2guid; FlowId=None |}
-                | :? ORMLamp as s -> {| FlowGuid=None; FlowId=s.FlowId |}
-                | _ -> failwith "ERROR"
-            
-            match dbx with
-            | :? Lamp    as d -> d.FlowGuid<-s.FlowGuid; d.FlowId<-s.FlowId
-            | :? NjLamp  as d -> d.FlowGuid<-s.FlowGuid |-> guid2str |> Option.toObj
-            | :? ORMLamp as d -> d.FlowId<-s.FlowId
-            | _ -> failwith "ERROR"
-
-        | (:? DsCondition) | (:? NjCondition) | (:? ORMCondition) ->
-            let s =
-                match sbx with
-                | :? DsCondition  as s -> {| FlowGuid=s.FlowGuid; FlowId=s.FlowId |}
-                | :? NjCondition  as s -> {| FlowGuid=s.FlowGuid |> Option.ofObj |-> s2guid; FlowId=None |}
-                | :? ORMCondition as s -> {| FlowGuid=None; FlowId=s.FlowId |}
-                | _ -> failwith "ERROR"
-            
-            match dbx with
-            | :? DsCondition  as d -> d.FlowGuid<-s.FlowGuid; d.FlowId<-s.FlowId
-            | :? NjCondition  as d -> d.FlowGuid<-s.FlowGuid |-> guid2str |> Option.toObj
-            | :? ORMCondition as d -> d.FlowId<-s.FlowId
-            | _ -> failwith "ERROR"
-
+        | (:? DsButton) | (:? NjButton) | (:? ORMButton)
+        | (:? Lamp) | (:? NjLamp) | (:? ORMLamp)
+        | (:? DsCondition) | (:? NjCondition) | (:? ORMCondition)
         | (:? DsAction) | (:? NjAction) | (:? ORMAction) ->
-            let s =
-                match sbx with
-                | :? DsAction  as s -> {| FlowGuid=s.FlowGuid; FlowId=s.FlowId |}
-                | :? NjAction  as s -> {| FlowGuid=s.FlowGuid |> Option.ofObj |-> s2guid; FlowId=None |}
-                | :? ORMAction as s -> {| FlowGuid=None; FlowId=s.FlowId |}
-                | _ -> failwith "ERROR"
-            
-            match dbx with
-            | :? DsAction  as d -> d.FlowGuid<-s.FlowGuid; d.FlowId<-s.FlowId
-            | :? NjAction  as d -> d.FlowGuid<-s.FlowGuid |-> guid2str |> Option.toObj
-            | :? ORMAction as d -> d.FlowId<-s.FlowId
-            | _ -> failwith "ERROR"
+            copyFlowProperties sbx dbx
 
         | ( :? ArrowBetweenCalls | :? ArrowBetweenWorks
           | :? ORMArrowCall | :? ORMArrowWork
